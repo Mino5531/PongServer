b99e0c1848560e6674b00dcf94b32d9bf96c16ec
Fhey <paffrath@yangnet.de>
2020-05-13 22:50:08 +0200

    DataSender changes

--- src/DataSender.py
+++ src/DataSender.py
@@ -1,4 +1,18 @@
+from DataBuffer import DataBuffer
+
+
+class ServerPackets:
+    s_EnterGame = 1
+
+
 class DataSender:
     @staticmethod
-    def SendGame(client):
-        pass
+    def SendGame(client, game):
+        buff = DataBuffer()
+        buff.WriteObject(ServerPackets.s_EnterGame)
+        if(client == game.client1):
+            buff.WriteObject(game.client2.name)
+        else:
+            buff.WriteObject(game.client1.name)
+        buff.WriteObject(game.gameID)
+        
