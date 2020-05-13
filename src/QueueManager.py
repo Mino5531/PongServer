7e8b84ab594e303ebda9682ae508110cea794c9d
Fhey <paffrath@yangnet.de>
2020-05-13 22:49:35 +0200

    Added queue functions

--- src/QueueManager.py
+++ src/QueueManager.py
@@ -1,5 +1,56 @@
+from time import sleep
+from Game import Game
+import threading
+
+
 class QueueManager:
     instance = None
+    __queueMutex = threading.Lock()
 
     def __init__(self):
         QueueManager.instance = self
+        self.queueRunning = True
+        self.queueThread = threading.Thread(target=self.ManageQueue, args=())
+        self.__queue = []
+        self.queueThread.setDaemon(True)
+        self.queueThread.start()
+        print("QueueManager initialized")
+# --------------------------------------------------------
+
+  ####            #  #   #
+  #               # #    #
+  ##   #  #  ###  ##     #
+  #    #  #  #    # #
+  #     ##   ###  #  #   #
+
+# --------------------------------------------------------
+    def AddToQueue(self, client):
+        QueueManager.__queueMutex.acquire()
+        try:
+            self.__queue.append(client)
+        finally:
+            QueueManager.__queueMutex.release()
+        print("%s is now in the pvp queue" % (client.name))
+
+    def RemoveFromQueue(self, client):
+        QueueManager.__queueMutex.acquire()
+        try:
+            for i in range(len(self.__queue)):
+                if(self.__queue[i] == client):
+                    self.__queue.pop(i)
+                    break
+        finally:
+            QueueManager.__queueMutex.release()
+
+    def ManageQueue(self):
+        while(self.queueRunning):
+            sleep(2)
+            QueueManager.__queueMutex.acquire()
+            try:
+                if(len(self.__queue) >= 2):
+                    while(len(self.__queue) >= 2):
+                        Game(self.__queue[0], self.__queue[1])
+                        self.__queue.pop(0)
+                        self.__queue.pop(0)
+            finally:
+                QueueManager.__queueMutex.release()
