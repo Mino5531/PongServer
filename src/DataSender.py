be754a9848f79864d631b3f407ca0b6becd7980e
Mino <paffrath@yangnet.de>
2020-05-14 12:07:51 +0200

    added isRight var to send on game set

--- src/DataSender.py
+++ src/DataSender.py
@@ -1,5 +1,5 @@
 from DataBuffer import DataBuffer
-
+from NetworkManager import NetworkManager
 
 class ServerPackets:
     s_EnterGame = 1
@@ -12,7 +12,9 @@ class DataSender:
         buff.WriteObject(ServerPackets.s_EnterGame)
         if(client == game.client1):
             buff.WriteObject(game.client2.name)
+            buff.WriteObject(True)
         else:
             buff.WriteObject(game.client1.name)
+            buff.WriteObject(False)
         buff.WriteObject(game.gameID)
-        
+        client.sock.send(buff.ToArray())
