ba35bf3eb231c6360cc467ae26832ed97c511cac
Fhey <paffrath@yangnet.de>
2020-05-16 01:28:08 +0200

    completed handleracketpos function

--- src/DataHandler.py
+++ src/DataHandler.py
@@ -19,6 +19,7 @@ class DataHandler:
                              ] = DataHandler.HandleNameSet
 
         DataHandler.handlers[ClientPackets.c_EnterPlayerQueue] = DataHandler.QueueEnterRequest
+        DataHandler.handlers[ClientPackets.c_RacketPositionUpdate] = DataHandler.HandleRacketPosUpdate
         print("DataHandler initialized")
 
     @staticmethod
@@ -34,8 +35,17 @@ class DataHandler:
         QueueManager.instance.AddToQueue(client)
 
     @staticmethod
-    def HandleRacketPosUpdate(data,client):
+    def HandleRacketPosUpdate(data, client):
         buff = DataBuffer(data)
         buff.SkipBytes(4)
-        x = buff.ReadFloat()
-        y = buff.ReadFloat()
+        up = buff.ReadBoolean()
+        if(client.game.client1 == client):
+            if(up):
+                client.game.LeftRacket.MoveUp()
+            else:
+                client.game.LeftRacket.MoveDown()
+        else:
+            if(up):
+                client.game.RightRacket.MoveUp()
+            else:
+                client.game.RightRacket.MoveDown()
