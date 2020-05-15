08b236000647e0ee39a11c0d353deb4f54508553
Fhey <paffrath@yangnet.de>
2020-05-16 01:27:07 +0200

    added racket and const

---
+++ src/Racket.py
@@ -0,0 +1,28 @@
+import const
+
+
+class Racket:
+    HEIGHT = 164
+    MAX_Y = const.SCREENHEIGHT-HEIGHT/2
+    MIN_Y = HEIGHT/2
+    SPEED = 5
+
+    def __init__(self, isRight):
+        self.y = Racket.MIN_Y
+        if(isRight):
+            self.x = 590
+        else:
+            self.x = 10
+
+    def MoveUp(self):
+        if(self.y - Racket.SPEED < Racket.HEIGHT/2):
+            return
+        self.y -= Racket.SPEED
+
+    def MoveDown(self):
+        if(self.y + Racket.SPEED > const.SCREENHEIGHT - Racket.HEIGHT/2):
+            return
+        self.y += Racket.SPEED
+
+    def Reset(self):
+        self.y = Racket.MIN_Y
